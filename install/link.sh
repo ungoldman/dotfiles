965947e7eb007e88d0bbcf898f87c07dcca81c11
Nate Goldman <ungoldman@gmail.com>
2017-03-27 16:54:40 -0700

    fix escaping in link script

--- install/link.sh
+++ install/link.sh
@@ -1,7 +1,7 @@
-
-
 cat <<EOT >> ~/.bash_profile
+
 # include dotfiles
-DOTFILES="${HOME}/dev/git/dotfiles"
-[ -f ${DOTFILES}/.bash_profile ] && source ${DOTFILES}/.bash_profile
+export DOTFILES="\${HOME}/dev/git/dotfiles"
+[ -f \${DOTFILES}/.bash_profile ] && source \${DOTFILES}/.bash_profile
+
 EOT
