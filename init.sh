1cfab8fe6dfcde75789ad0d5c8d4258b15adc4ef
Nate Goldman <ungoldman@gmail.com>
2017-03-27 17:11:00 -0700

    rename link to init, use magic

--- init.sh
+++ init.sh
@@ -1,7 +1,11 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 cat <<EOT >> ~/.bash_profile
 
 # include dotfiles
-export DOTFILES="\${HOME}/dev/git/dotfiles"
+export DOTFILES="${DIR}"
 [ -f \${DOTFILES}/.bash_profile ] && source \${DOTFILES}/.bash_profile
 
 EOT
+
+unset DIR
