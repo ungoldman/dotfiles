f27cfa772258df2bc0c4a23cac59d925dd5442a9
Nate Goldman <427322+ungoldman@users.noreply.github.com>
2022-10-26 15:36:13 -0700

    misc updates

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,5 +1,7 @@
-dcolor=$PS_GREEN_BOLD
+dcolor=$PS_GREEN
 remote=""
+last_cmd_status="\[\$(if [ \$? == 0 ]; then echo -e \"\\e[0;92m\"; else echo -e \"\\e[0;91m\"; fi)\]∴\[\e[0m\] "
+reset="\[\e[0m\]"
 
 if [ ${UID} -eq 0 ]; then
   dcolor=$PS_RED_BOLD
@@ -9,6 +11,6 @@ if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
   remote="${PS_RED}☎  ${PS_CYAN}\u@\h "
 fi
 
-export PS1="${remote}${PS_GREEN}\w ${PS_RED}\$(parse_git_branch)${dcolor}∴ ${PS_WHITE}"
+export PS1="${remote}${last_cmd_status}${PS_PURPLE}\w ${PS_RED}\$(parse_git_branch)${dcolor}$ ${reset}"
 
 unset dcolor client_loc
