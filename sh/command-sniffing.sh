95f5bbf25955348068e6db41b2e719a096c4f6d9
Nate Goldman <427322+ungoldman@users.noreply.github.com>
2023-01-20 16:42:42 -0800

    fix ding dang quotes

--- sh/command-sniffing.sh
+++ sh/command-sniffing.sh
@@ -1,3 +1,3 @@
 # zsh command sniffing
-alias list-ext-commands='print -rlo -- $commands:t | less'
-alias list-all-commands='type -m '*' | less'
+alias list-ext-commands="print -rlo -- $commands:t | less"
+alias list-all-commands="type -m '*' | less"
