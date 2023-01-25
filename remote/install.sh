89f1bf1262dd2377606aae2235a8720700ebcea3
Nate Goldman <427322+ungoldman@users.noreply.github.com>
2023-01-25 12:23:38 -0800

    remote install script

--- remote/install.sh
+++ remote/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-# install script: /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ungoldman/dotfiles/HEAD/remote/install.sh)"
+echo "Copying .ungoldmanrc to home directory"
 
-echo "WIP"
+# add .ungoldmanrc file to home directory
+curl https://raw.githubusercontent.com/ungoldman/dotfiles/HEAD/remote/.ungoldmanrc > ~/.ungoldmanrc
+
+# add source line in bashrc unless it's already there
+if ! grep -q "source .ungoldmanrc" .bashrc; then
+  echo "Adding .ungoldmanrc to .bashrc"
+  echo $'\n# ungoldman remote helpers\nsource .ungoldmanrc' >> .bashrc
+fi
