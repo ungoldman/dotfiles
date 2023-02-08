ea8610b33a97334e995d5a70179babe99fdf3184
Nate Goldman <427322+ungoldman@users.noreply.github.com>
2023-02-08 11:21:39 -0800

    merge files for count fns

--- sh/extcount.sh
+++ sh/extcount.sh
@@ -2,3 +2,18 @@
 function extcount {
   find . -iname "*.$*" | cut -d . -f1 | uniq -c
 }
+
+# count js files and laugh/cry
+function extcountjs {
+  cat << END
+
+    .wwwwwwww.
+  .w"  "WW"  "w.
+ ."   /\  /\   ".
+|\     o  o     /|
+ \|  ___\/___  |/
+   \ \_v__v_/ /     $(find . -iname "*.js" ! -path '*/node_modules/*' | cut -d . -f1 | uniq -c | xargs)!
+    \________/      ha, ha, ha
+
+END
+}
