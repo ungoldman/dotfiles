b40d1ba6f556b1f46ef7cb3c9ed8ff724668c7ca
Nate Goldman <427322+ungoldman@users.noreply.github.com>
2024-01-10 13:02:35 -0800

    add gcm alias from oh-my-zsh git plugin

--- sh/git.sh
+++ sh/git.sh
@@ -3,7 +3,7 @@ alias g='git'
 alias ga='git add'
 alias gc='git commit'
 alias gca='git commit --amend'
-alias gcm='git commit -m'
+alias gcm='git checkout $(git_main_branch)'
 alias gd='git diff'
 alias gds='git diff --staged'
 
@@ -13,3 +13,21 @@ alias galp='git alias'
 # hub
 alias h='hub'
 alias b='hub browse'
+
+# borrowed from oh-my-zsh
+# https://github.com/ohmyzsh/ohmyzsh/blob/1c8dee848dad169c9eb1a7b0148842b48a99cad1/plugins/git/git.plugin.zsh#L35
+# Check if main exists and use instead of master
+function git_main_branch() {
+  command git rev-parse --git-dir &>/dev/null || return
+  local ref
+  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,master}; do
+    if command git show-ref -q --verify $ref; then
+      echo ${ref:t}
+      return 0
+    fi
+  done
+
+  # If no main branch was found, fall back to master but return error
+  echo master
+  return 1
+}
