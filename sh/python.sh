87269b58e57456ac21d6f5dbff9247294eee70a3
Nate Goldman <427322+ungoldman@users.noreply.github.com>
2024-01-18 14:57:16 -0800

    pyenv

--- sh/python.sh
+++ sh/python.sh
@@ -1,3 +1,4 @@
 # unversioned symlinks
 # https://github.com/Homebrew/brew/blob/master/docs/Homebrew-and-Python.md#python-3y
-export PATH="$(brew --prefix python)/libexec/bin:$PATH"
+# export PATH="$(brew --prefix python)/libexec/bin:$PATH"
+# use pyenv instead?
