255218c8f7a3fe78abfa416e3f8aa8e99a3ba590
Nate Goldman <427322+ungoldman@users.noreply.github.com>
2024-02-07 10:41:45 -0800

    simplify git aliases

--- sh/git.sh
+++ sh/git.sh
@@ -1,11 +1,5 @@
 # git
 alias g='git'
-alias ga='git add'
-alias gc='git commit'
-alias gca='git commit --amend'
-alias gcm='git checkout $(git_main_branch)'
-alias gd='git diff'
-alias gds='git diff --staged'
 
 # list all git aliases
 alias galp='git alias'
