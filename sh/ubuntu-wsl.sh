ecb0e2a36f328fa3d8eb9064b41a32cf711425d5
Nate Goldman <427322+ungoldman@users.noreply.github.com>
2026-07-05 21:15:37 -0700

    wsl: only apply ubuntu-wsl.sh inside WSL

--- sh/ubuntu-wsl.sh
+++ sh/ubuntu-wsl.sh
@@ -1,3 +1,6 @@
+# everything here is WSL-only, bail on other systems
+[[ -n "$WSL_DISTRO_NAME" ]] || return 0
+
 # nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
